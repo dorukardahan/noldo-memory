6108ecebe61933ba6e085a5b6ad31a9086ceea58
semantic-release <semantic-release>
2026-04-25 21:54:39 +0000

    chore(release): 1.27.0

--- plugin/index.js
+++ plugin/index.js
@@ -25,7 +25,7 @@ import {
   registerNativeLifecycleCapture,
 } from "./src/hooks.js";
 
-export const NOLDOMEM_PLUGIN_VERSION = "1.26.0";
+export const NOLDOMEM_PLUGIN_VERSION = "1.27.0";
 
 export default definePluginEntry({
   id: "noldomem",
